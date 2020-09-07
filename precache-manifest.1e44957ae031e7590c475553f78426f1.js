810f9bb9f809f05db35aa7838ee8f94aa461fdd3
Sahil Aggarwal <sahil2.aggarwal@paytm.com>
2020-09-07 14:13:58 +0530

    Updates

---
+++ precache-manifest.1e44957ae031e7590c475553f78426f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1368a097a22fec5028397798b493d5",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "d75b4553eb46bc22f812",
+    "url": "/kanban-board/static/css/main.b0cdade6.chunk.css"
+  },
+  {
+    "revision": "e7a6a2849692ccfe4858",
+    "url": "/kanban-board/static/js/2.0e15ea99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kanban-board/static/js/2.0e15ea99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75b4553eb46bc22f812",
+    "url": "/kanban-board/static/js/main.83fde4f7.chunk.js"
+  },
+  {
+    "revision": "ba2096fa3b770a19f0f1",
+    "url": "/kanban-board/static/js/runtime-main.8e469a6a.js"
+  }
+]);
